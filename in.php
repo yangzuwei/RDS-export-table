1b29ccd721df30b2b0050e1ddd1216e68b99a6c2
杨祖威 <316193839@qq.coml>
2018-07-03 15:37:14 +0800

    prepare bind params

--- in.php
+++ in.php
@@ -17,31 +17,96 @@ $insertData = [];
 
 $fd = fopen($file, 'r');
 
-$mysqli->begin_transaction();
+//$mysqli->begin_transaction();
 $mysqli->query('truncate `' . $table . '`');
-$first = json_decode(fgets($fd), true);;
-$fields = implode(',', array_keys($first));
+$first = json_decode(fgets($fd), true);
+$fieldsArray = array_keys($first);
 
+//trans the data type
+$typesSql = 'show COLUMNS from `' . $table . '`';
+$typeStmt = $mysqli->query($typesSql);
+$fieldType = [];
+$placeHolder = [];
+$bindTypes = $paramType = '';
+
+
+while ($types = $typeStmt->fetch_object()) {
+    $fields[] = $types->Field;
+    $placeHolder[] = '?';
+    $fieldType[$types->Field] = $types->Type;
+
+    if (strpos($types->Type, 'int')) {
+        $paramType = 'i';
+    } elseif (strpos($types->Type, 'double') || strpos($types->Type, 'decimal') || strpos($types->Type, 'float')) {
+        $paramType = 'd';
+    } else {
+        $paramType = 's';
+    }
+
+    $bindTypes .= $paramType;
+
+    if ($types->Null == 'YES') {
+        $fieldDefault[$types->Field] = $types->Default;
+    }
+}
+
+$fields = implode(',', $fields);
+$placeHolder = "(" . implode(",", $placeHolder) . ")";
+//var_dump($bindTypes);exit();
 rewind($fd);
-
+$i = 0;
+echo "start ... \n";
+$time = time();
 while (!feof($fd)) {
     if ($content = fgets($fd)) {
-        $insertData[] = "('" . implode("','", json_decode($content, true)) . "')";
-    }
-    if (count($insertData) >= 10000) {
-        $datas = implode(',', $insertData);
-        $mysqli->query('insert into `' . $table . '`(' . $fields . ') values' . $datas);
-        unset($insertData);
+        $content = json_decode($content, true);
+        $content = transDataType($fieldType, $content, $fieldDefault);
+        $insertContent = implode(',',$content);
+        //var_dump($content);exit();
+        $insertSql = 'insert into `' . $table . '` (' . $fields . ') values(' . $insertContent.')';
+        if($mysqli->query($insertSql) === false){
+            echo $insertSql."\n";
+        }
     }
 }
 
-if (!empty($insertData)) {
-    $datas = implode(',', $insertData);
-    $insertSql = 'insert into `' . $table . '` (' . $fields . ') values' . $datas;
-    $mysqli->query($insertSql);
-}
-
-$mysqli->commit();
-echo "done \n";
+echo "done " . (time() - $time) . " \n";
 fclose($fd);
 $mysqli->close();
+
+
+function transDataType($fieldType, $data, $fieldDefault)
+{
+    $shouldTransToNumber = ['int', 'double', 'decimal'];
+    foreach ($data as $key => $value) {
+        $condition = $tmpCondition = false;
+        foreach ($shouldTransToNumber as $dataBaseType) {
+            $tmpCondition = strstr($fieldType[$key], $dataBaseType);
+            //var_dump($fieldType[$key], $dataBaseType,$tmpCondition);
+            $condition = $condition || $tmpCondition;
+        }
+
+        if ($condition) {
+            $data[$key] = $value * 1;
+        } elseif ($value == '') {
+            $data[$key] = $fieldDefault[$key] === NULL ? 'null' : $fieldDefault[$key];
+        } else {
+            $data[$key] = "'" . str_replace("'",'"',$value) . "'";
+        }
+    }
+
+    return $data;
+
+}
+
+function refValues($arr)
+{
+    if (strnatcmp(phpversion(), '5.3') >= 0) //Reference is required for PHP 5.3+
+    {
+        $refs = array();
+        foreach ($arr as $key => $value)
+            $refs[$key] = &$arr[$key];
+        return $refs;
+    }
+    return $arr;
+}
